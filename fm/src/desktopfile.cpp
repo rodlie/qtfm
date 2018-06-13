332f1a89e0f84148c17ff4107b1c8e5ae8e6ad53
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2018-06-13 18:55:30 +0200

    freedesktop: support NoDisplay
    Signed-off-by: Ole-André Rodlie <ole.andre.rodlie@gmail.com>

--- fm/src/desktopfile.cpp
+++ fm/src/desktopfile.cpp
@@ -23,6 +23,7 @@ DesktopFile::DesktopFile(const QString &fileName) {
   exec = desktop.value("Exec", "").toString();
   icon = desktop.value("Icon", "").toString();
   type = desktop.value("Type", "Application").toString();
+  no_display = desktop.value("NoDisplay", false).toBool();
   terminal = desktop.value("Terminal", false).toBool();
   categories = desktop.value("Categories").toString().remove(" ").split(";");
   mimeType = desktop.value("MimeType").toString().remove(" ").split(";");
@@ -61,6 +62,11 @@ QString DesktopFile::getIcon() const {
 
 QString DesktopFile::getType() const {
     return type;
+}
+
+bool DesktopFile::noDisplay() const
+{
+    return no_display;
 }
 
 bool DesktopFile::isTerminal() const
