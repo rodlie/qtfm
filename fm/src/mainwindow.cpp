9d4bb0fa41c9be4e1c8d58a80f34f4174715bd5b
kuba <kuba456@gmail.com>
2019-01-26 13:25:59 -0800

    Addressed 4.8 compatibility by supporting the tab completion from 5.0 up.

--- fm/src/mainwindow.cpp
+++ fm/src/mainwindow.cpp
@@ -152,7 +152,9 @@ MainWindow::MainWindow()
     dockBookmarks->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
     bookmarksList = new QListView(dockBookmarks);
     bookmarksList->setMinimumHeight(24); // Docks get the minimum size from their content widget
-    bookmarksList->setFocusPolicy(Qt::FocusPolicy::NoFocus); // Avoid hijaking foucs when Tab on Edit Path
+#if QT_VERSION >= 0x050000
+     bookmarksList->setFocusPolicy(Qt::FocusPolicy::NoFocus); // Avoid hijacking focus when Tab on Edit Path
+#endif
     dockBookmarks->setWidget(bookmarksList);
     addDockWidget(Qt::LeftDockWidgetArea, dockBookmarks);
 
@@ -1488,7 +1490,7 @@ void MainWindow::contextMenuEvent(QContextMenuEvent * event) {
         isMedia = bookmarksList->currentIndex().data(MEDIA_MODEL).toBool();
         if (!isMedia) {
             popup->addAction(delBookmarkAct);
-            popup->addAction(editBookmarkAct);  //icon
+            popup->addAction(editBookmarkAct);	//icon
         } else {
             // media actions
 #ifndef NO_UDISKS
@@ -1504,7 +1506,7 @@ void MainWindow::contextMenuEvent(QContextMenuEvent * event) {
         }
       } else {
         bookmarksList->clearSelection();
-        popup->addAction(addSeparatorAct);  //seperator
+        popup->addAction(addSeparatorAct);	//seperator
         popup->addAction(wrapBookmarksAct);
       }
       popup->addSeparator();
@@ -1622,8 +1624,8 @@ bool MainWindow::eventFilter(QObject *o, QEvent *e)
         default:;
         }
     }
-
-      if (dynamic_cast<QListView*>(o) != nullptr ){
+#if QT_VERSION >= 0x050000
+      if (dynamic_cast<QListView*>(o) != NULL ){
         if (e->type()==QEvent::KeyPress) {
             QKeyEvent* key = static_cast<QKeyEvent*>(e);
             if ( (key->key()==Qt::Key_Tab) ) {
@@ -1647,7 +1649,7 @@ bool MainWindow::eventFilter(QObject *o, QEvent *e)
             }
         }
     }
-
+#endif
     return QMainWindow::eventFilter(o, e);
 }
 
