a088d008b35ca6e1188f464216a42e806f341f11
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-02-08 14:39:37 +0100

    launcher:
    - support open .desktop files from cmdline
    - support open file in default app from cmdline

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -20,12 +20,50 @@
  */
 
 #include "dialog.h"
+#include "desktopfile.h"
+#include "mimeutils.h"
+#include "common.h"
+
 #include <QApplication>
+#include <QFile>
+#include <QFileInfo>
+#include <QProcess>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Dialog d;
-    d.show();
-    return a.exec();
+    if (argc>1) {
+        QString fileName = argv[1];
+        if (QFile::exists(fileName)) {
+            if (fileName.endsWith(QString(".desktop"))) { // open app
+                DesktopFile desktop(fileName);
+                if (!desktop.getExec().isEmpty()) {
+                    QProcess::startDetached(desktop.getExec()
+                                            .split(QString(" "))
+                                            .takeFirst());
+                    return 0;
+                }
+            } else { // open file
+                MimeUtils mimeUtils;
+                QString mime = mimeUtils.getMimeType(fileName);
+                if (!mime.isEmpty()) {
+                    QStringList appNames = mimeUtils.getDefault(mime);
+                    DesktopFile desktop(Common::findApplication(qApp->applicationFilePath(),
+                                                                appNames.at(0)));
+                    if (!desktop.getExec().isEmpty()) {
+                        QFileInfo fileInfo(fileName);
+                        mimeUtils.openInApp(desktop.getExec(),
+                                            fileInfo,
+                                            QString());
+                        return 0;
+                    }
+                }
+            }
+        }
+        return 1;
+    } else {
+        Dialog d;
+        d.show();
+        return a.exec();
+    }
 }
